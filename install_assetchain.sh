57cc2fae1c3871880dd3ee83f2d7c8b83ddadb92
Jorian <jorian@outlook.com>
2018-03-03 20:43:47 +0100

    Added install script for assetchains

---
+++ install_assetchain.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+COIN=$1
+RPCUSER=$2
+RPCPASSWORD=$3
+RPCPORTASSET=$4
+
+let "RPCPORTELECTRUM = 3 + $RPCPORTASSET"
+
+echo $RPCPORTELECTRUM
+HOSTIP=$5
+TCPPORT=$6
+
+sudo add-apt-repository ppa:jonathonf/python-3.6
+sudo apt-get update
+sudo apt-get install python3-setuptools python3.6 python3.6-dev libleveldb-dev --yes
+
+sudo apt-get install python3-pip --yes
+sudo pip3 install multidict
+
+git clone https://github.com/chainmakers/electrumx
+cd electrumx
+
+sudo python3.6 setup.py install
+
+sudo cp contrib/systemd/electrumx.service /etc/systemd/system/electrumx_$COIN.service
+
+sudo sed -i -e 's/'"Description=Electrumx"'/'"Description=Electrumx_$COIN"'/g' /etc/systemd/system/electrumx_$COIN.service
+sudo sed -i -e 's|'"EnvironmentFile=/etc/electrumx.conf"'|'"EnvironmentFile=/etc/electrumx_$COIN.conf"'|g' /etc/systemd/system/electrumx_$COIN.service
+sudo sed -i -e 's/'"User=electrumx"'/'"User=$USER"'/g' /etc/systemd/system/electrumx_$COIN.service
+
+mkdir ~/electrumdb_$COIN
+
+sudo touch /etc/electrumx_$COIN.conf
+
+echo "COIN = Komodo" | sudo tee --append /etc/electrumx_$COIN.conf
+echo "DB_DIRECTORY = /home/$USER/electrumdb_$COIN" | sudo tee --append /etc/electrumx_$COIN.conf
+echo "DAEMON_URL = http://$RPCUSER:$RPCPASSWORD@127.0.0.1:$RPCPORTASSET/" | sudo tee --append /etc/electrumx_$COIN.conf
+echo "RPC_HOST = 127.0.0.1" | sudo tee --append /etc/electrumx_$COIN.conf
+echo "RPC_PORT = $RPCPORTELECTRUM" | sudo tee --append /etc/electrumx_$COIN.conf
+echo "HOST = 127.0.0.1, $HOSTIP" | sudo tee --append /etc/electrumx_$COIN.conf
+echo "TCP_PORT = $TCPPORT" | sudo tee --append /etc/electrumx_$COIN.conf
+echo "EVENT_LOOP_POLICY = uvloop" | sudo tee --append /etc/electrumx_$COIN.conf
+echo "PEER_DISCOVERY = self" | sudo tee --append /etc/electrumx_$COIN.conf
+
+sudo systemctl start electrumx_$COIN
+
